c96176f34206efa8a8e78d3be0b067d5dbbbf112
Greezlyyy <87074517+Greezlyyy@users.noreply.github.com>
2021-12-23 22:56:51 +0100

    Bot

--- index.js
+++ index.js
@@ -68,9 +68,33 @@ client.on("message", (message) => {
     }
 })
 
-client.on("guildMemberAdd", member => {
-    var canale = client.channels.cache.get("923693673168916510")
-    canale.setParent("Members: " + member.guild.memberCount)
+const ytch = require("yt-channel-info") //npm i yt-channel-info
+
+setInterval(function () {
+    var canale = client.channels.cache.get("923695088415166567")
+    ytch.getChannelInfo("UCttG55w_E2AIj63lR_v1VrQ")
+        .then(response => {
+            canale.setName(`🧑Subscribers: ${response.subscriberCount}`)
+        })
+}, 1000 * 60)
+
+client.on("messageCreate", message => {
+    if (message.content == "!lastvideo") {
+        ytch.getChannelVideos("UCttG55w_E2AIj63lR_v1VrQ")
+            .then(response => {
+                var embed = new Discord.MessageEmbed()
+                    .setTitle("Last video")
+                    .setDescription("Ultimo video uscito sul canale")
+                    .addField("Title", response.items[0].title)
+                    .addField("Link", "https://www.youtube.com/watch?v=" + response.items[0].videoId)
+                    .addField("Views", response.items[0].viewCount.toString())
+                    .addField("Duration", response.items[0].durationText)
+                    .addField("Published", response.items[0].publishedText)
+                    .setImage(response.items[0].videoThumbnails[3].url)
+
+                message.channel.send({ embeds: [embed] })
+            })
+    }
 })
 
 client.on("message", message => {

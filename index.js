b27b980188f9648d0cdabb9ec5fdc693bbb0cf2a
Greezlyyy <87074517+Greezlyyy@users.noreply.github.com>
2021-12-23 18:10:36 +0100

    Yessir

--- index.js
+++ index.js
@@ -195,18 +195,3 @@ client.on("message", message => {
         }
     }
 })
-
-var embed = new Discord.MessageEmbed()
-    .setColor("BLUE")
-    .setTitle("Regole")
-    .setDescription(`✘ Nessun contenuto illegale.
-    ✘ Nessun razzismo.
-    ✘ Nessun tipo di spam.
-    ✘ Nessuna pubblicità/link senza permesso.`)
-
-//regole
-client.on("message", message => {
-    if (message.content == "!regole") {
-        message.channel.send(embed)
-    }
-})

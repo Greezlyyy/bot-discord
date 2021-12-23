2d77aecb90c7b4b66d246874ee846ea81dc0b287
Greezlyyy <87074517+Greezlyyy@users.noreply.github.com>
2021-12-23 19:14:10 +0100

    ok lets go

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ client.on("message", message => {
 //BENVENUTO
 client.on("guildMemberAdd", (member) => {
     //console.log(member.guild); Per avere tutte le info del utente e del server
-    client.channels.cache.get("921819183363264583").send("Ciao " + member.toString() + " benvunuto in **" + member.guild.name + "**\rSei il **" + member.guild.memberCount + "° membro**");
+    client.channels.cache.get("921819183363264583").send("Benvenuto " + member.toString() + " in **" + member.guild.name + "**\rSei il **" + member.guild.memberCount + "° membro**");
 })
 
 //ADDIO
 client.on("guildMemberRemove", (member) => {
-    client.channels.cache.get("921819183363264583").send("Ciao ciao" + member.toString() + ", torna presto!");
+    client.channels.cache.get("921819183363264583").send("Arrivederci " + member.toString() + ", torna presto!");
 })
 
 var embed = new Discord.MessageEmbed()
